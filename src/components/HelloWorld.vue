f9e8cdf527df32f00caac02182aadea4e091e7e1
khoiph <khoiph@devblock.net>
2020-08-21 13:05:59 +0700

    push

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+   .hello {
+     width: 20%;
+     margin: auto;
+     background-color: red
+   }
+</style>
